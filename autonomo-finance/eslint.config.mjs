a5c6e77d325899eb996d6565aec7b2d31a9c20b9
raffaew <raffa96dias@gmail.com>
2026-05-27 09:07:51 -0300

    Add missing TypeScript ESLint import to configuration

--- autonomo-finance/eslint.config.mjs
+++ autonomo-finance/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript";
+import tseslint from "eslint-config-next/typescript.js";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
