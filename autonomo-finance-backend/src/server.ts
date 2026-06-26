d82ca2faee203795af07ffdf78f32ce172bd7300
raffaew <raffa96dias@gmail.com>
2026-06-26 10:32:41 -0300

    fix: correct import order in server.ts for improved readability

--- autonomo-finance-backend/src/server.ts
+++ autonomo-finance-backend/src/server.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
+import app from './app';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-import app from './app';
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));   
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
